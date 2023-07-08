7aee8a8f5906f1851ef8b7d6fd0c2c358b9e02d3
Jayson Grace <jayson.e.grace@gmail.com>
2023-07-07 22:52:18 -0600

    Update magefile

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -4,15 +4,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 
-	"github.com/fatih/color"
-	goutils "github.com/l50/goutils"
-
-	// mage utility functions
-	"github.com/magefile/mage/mg"
+	"github.com/l50/goutils/v2/dev/lint"
+	mageutils "github.com/l50/goutils/v2/dev/mage"
+	"github.com/l50/goutils/v2/docs"
+	fileutils "github.com/l50/goutils/v2/file/fileutils"
+	"github.com/l50/goutils/v2/git"
+	"github.com/l50/goutils/v2/sys"
 	"github.com/magefile/mage/sh"
+	"github.com/spf13/afero"
 )
 
 func init() {
@@ -21,55 +24,79 @@ func init() {
 
 // InstallDeps Installs go dependencies
 func InstallDeps() error {
-	fmt.Println(color.YellowString("Installing dependencies."))
+	fmt.Println("Installing dependencies.")
 
-	if err := goutils.Tidy(); err != nil {
-		return fmt.Errorf(color.RedString(
-			"failed to install dependencies: %v", err))
+	if err := mageutils.Tidy(); err != nil {
+		return fmt.Errorf("failed to install dependencies: %v", err)
 	}
 
-	if err := goutils.InstallGoPCDeps(); err != nil {
-		return fmt.Errorf(color.RedString(
-			"failed to install pre-commit dependencies: %v", err))
+	if err := lint.InstallGoPCDeps(); err != nil {
+		return fmt.Errorf("failed to install pre-commit dependencies: %v", err)
 	}
 
-	if err := goutils.InstallVSCodeModules(); err != nil {
-		return fmt.Errorf(color.RedString(
-			"failed to install vscode-go modules: %v", err))
+	if err := mageutils.InstallVSCodeModules(); err != nil {
+		return fmt.Errorf("failed to install vscode-go modules: %v", err)
 	}
 
 	return nil
 }
 
-// InstallPreCommitHooks Installs pre-commit hooks locally
-func InstallPreCommitHooks() error {
-	mg.Deps(InstallDeps)
+// FindExportedFuncsWithoutTests finds exported functions without tests
+func FindExportedFuncsWithoutTests(pkg string) ([]string, error) {
+	funcs, err := mageutils.FindExportedFuncsWithoutTests(os.Args[1])
 
-	fmt.Println(color.YellowString("Installing pre-commit hooks."))
-	if err := goutils.InstallPCHooks(); err != nil {
-		return err
+	if err != nil {
+		log.Fatalf("failed to find exported functions without tests: %v", err)
 	}
+
+	for _, funcName := range funcs {
+		fmt.Println(funcName)
+	}
+
+	return funcs, nil
+
+}
+
+// GeneratePackageDocs generates package documentation
+// for packages in the current directory and its subdirectories.
+func GeneratePackageDocs() error {
+	fs := afero.NewOsFs()
+
+	repoRoot, err := git.RepoRoot()
+	if err != nil {
+		return fmt.Errorf("failed to get repo root: %v", err)
+	}
+	sys.Cd(repoRoot)
+
+	repo := docs.Repo{
+		Owner: "l50",
+		Name:  "awsutils",
+	}
+
+	template := filepath.Join(repoRoot, "magefiles", "tmpl", "README.md.tmpl")
+	if err := docs.CreatePackageDocs(fs, repo, template); err != nil {
+		return fmt.Errorf("failed to create package docs: %v", err)
+	}
+
+	fmt.Println("Package docs created.")
 
 	return nil
 }
 
 // RunPreCommit runs all pre-commit hooks locally
 func RunPreCommit() error {
-	mg.Deps(InstallDeps)
-
-	fmt.Println(color.YellowString("Updating pre-commit hooks."))
-	if err := goutils.UpdatePCHooks(); err != nil {
+	fmt.Println("Updating pre-commit hooks.")
+	if err := lint.UpdatePCHooks(); err != nil {
 		return err
 	}
 
-	fmt.Println(color.YellowString(
-		"Clearing the pre-commit cache to ensure we have a fresh start."))
-	if err := goutils.ClearPCCache(); err != nil {
+	fmt.Println("Clearing the pre-commit cache to ensure we have a fresh start.")
+	if err := lint.ClearPCCache(); err != nil {
 		return err
 	}
 
-	fmt.Println(color.YellowString("Running all pre-commit hooks locally."))
-	if err := goutils.RunPCHooks(); err != nil {
+	fmt.Println("Running all pre-commit hooks locally.")
+	if err := lint.RunPCHooks(); err != nil {
 		return err
 	}
 
@@ -77,34 +104,87 @@ func RunPreCommit() error {
 }
 
 // RunTests runs all of the unit tests
-func RunTests() error {
-	mg.Deps(InstallDeps)
-
-	fmt.Println(color.YellowString("Running unit tests."))
-	if err := sh.RunV(filepath.Join(".hooks", "go-unit-tests.sh"), "all"); err != nil {
-		return fmt.Errorf(color.RedString("failed to run unit tests: %v", err))
+func RunTests(testType string) error {
+	if testType == "" {
+		testType = "coverage"
+	}
+	fmt.Println("Running unit tests.")
+	if err := sh.RunV(filepath.Join(".hooks", "go-unit-tests.sh"), testType); err != nil {
+		return fmt.Errorf("failed to run unit tests: %v", err)
 	}
 
 	return nil
 }
 
-// UpdateMirror updates pkg.go.dev with the release associated with the input tag
+// UpdateMirror updates pkg.go.dev and proxy.golang.org with the
+// release associated with the input tag
 func UpdateMirror(tag string) error {
 	var err error
-	fmt.Println(color.YellowString("Updating pkg.go.dev with the new tag %s.", tag))
+	fmt.Printf("Updating pkg.go.dev with the new tag %s.", tag)
 
-	err = sh.RunV("curl", "--silent", fmt.Sprintf(
-		"https://sum.golang.org/lookup/github.com/l50/goutils@%s",
-		tag))
-	if err != nil {
-		return fmt.Errorf(color.RedString("failed to update proxy.golang.org: %w", err))
+	repo := docs.Repo{
+		Owner: "l50",
+		Name:  "awsutils",
 	}
 
 	err = sh.RunV("curl", "--silent", fmt.Sprintf(
-		"https://proxy.golang.org/github.com/l50/goutils/@v/%s.info",
-		tag))
+		"https://sum.golang.org/lookup/github.com/%s/%s@%s",
+		repo.Owner, repo.Name, tag))
 	if err != nil {
-		return fmt.Errorf(color.RedString("failed to update pkg.go.dev: %w", err))
+		return fmt.Errorf("failed to update proxy.golang.org: %w", err)
+	}
+
+	err = sh.RunV("curl", "--silent", fmt.Sprintf(
+		"https://proxy.golang.org/github.com/%s/%s/@v/%s.info",
+		repo.Owner, repo.Name, tag))
+	if err != nil {
+		return fmt.Errorf("failed to update pkg.go.dev: %w", err)
+	}
+
+	return nil
+}
+
+// UpdateDocs updates the package documentation
+// for packages in the current directory and its subdirectories.
+func UpdateDocs() error {
+	repo := docs.Repo{
+		Owner: "l50",
+		Name:  "awsutils",
+	}
+
+	fs := afero.NewOsFs()
+
+	templatePath := filepath.Join("magefiles", "tmpl", "README.md.tmpl")
+
+	if err := docs.CreatePackageDocs(fs, repo, templatePath); err != nil {
+		return fmt.Errorf("failed to update docs: %v", err)
+	}
+
+	return nil
+}
+
+// UseFixCodeBlocks fixes code blocks for the input filepath
+// using the input language.
+//
+// **Parameters:**
+//
+// filepath: the path to the file or directory to fix
+// language: the language of the code blocks to fix
+//
+// **Returns:**
+//
+// error: an error if one occurred
+//
+// Example:
+//
+// ```go
+// mage fixcodeblocks docs/docGeneration.go go
+// ```
+func UseFixCodeBlocks(filepath string, language string) error {
+	file := fileutils.RealFile(filepath)
+
+	if err := docs.FixCodeBlocks(file, language); err != nil {
+		return fmt.Errorf("failed to fix code blocks: %v", err)
 	}
 
 	return nil
