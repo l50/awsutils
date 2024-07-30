e1d5beb9300e4b7ad8afb78b947023b6a2857c23
Jayson Grace <jayson.e.grace@gmail.com>
2024-07-30 13:12:02 -0600

    Added IAM role management functionality with tests
    **Added:**
    
    - Implemented IAM role management functions:
      - `CreateRole`, `AttachRolePolicy`, `PutRolePolicy`, `CreateInstanceProfile`
      - `AddRoleToInstanceProfile`, `DetachRolePolicy`, `DeleteRolePolicy`
      - `RemoveRoleFromInstanceProfile`, `DeleteInstanceProfile`, `DeleteRole`
    - Added documentation for the new IAM role functions.
    - New unit tests for the IAM role functions using mock clients.
    - Created automated tests for IAM functions:
      - `CreateRole`, `AttachRolePolicy`, `PutRolePolicy`, `CreateInstanceProfile`
      - `AddRoleToInstanceProfile`, `DetachRolePolicy`, `DeleteRolePolicy`
      - `RemoveRoleFromInstanceProfile`, `DeleteInstanceProfile`, `DeleteRole`
    
    **Changed:**
    
    - Updated `IdentityClientAPI` to include new IAM role management methods.
    - Refactored existing tests to incorporate new IAM role functions.
    - Improved test coverage for IAM-related functionalities.
    
    **Removed:**
    
    - Removed static role and profile names from function parameters in favor of dynamically generated names.

--- iam/README.md
+++ iam/README.md
@@ -18,6 +18,163 @@ designed to simplify common iam tasks.
 
 ## Functions
 
+### AWSService.AddRoleToInstanceProfile(string)
+
+```go
+AddRoleToInstanceProfile(string) *iam.AddRoleToInstanceProfileOutput, error
+```
+
+AddRoleToInstanceProfile adds a role to an instance profile.
+
+**Parameters:**
+
+profileName: The name of the instance profile to add the role to.
+roleName: The name of the role to add to the instance profile.
+
+**Returns:**
+
+*iam.AddRoleToInstanceProfileOutput: A pointer to the AddRoleToInstanceProfileOutput.
+error: An error if any issue occurs while trying to add the role to the instance profile.
+
+---
+
+### AWSService.AttachRolePolicy(string)
+
+```go
+AttachRolePolicy(string) *iam.AttachRolePolicyOutput, error
+```
+
+AttachRolePolicy attaches a policy to a role.
+
+**Parameters:**
+
+roleName: The name of the role to attach the policy to.
+policyArn: The ARN of the policy to attach.
+
+**Returns:**
+
+*iam.AttachRolePolicyOutput: A pointer to the AttachRolePolicyOutput.
+error: An error if any issue occurs while trying to attach the policy to the role.
+
+---
+
+### AWSService.CreateInstanceProfile(string)
+
+```go
+CreateInstanceProfile(string) *iam.CreateInstanceProfileOutput, error
+```
+
+CreateInstanceProfile creates a new instance profile with the given name.
+
+**Parameters:**
+
+profileName: The name of the instance profile to create.
+
+**Returns:**
+
+*iam.CreateInstanceProfileOutput: A pointer to the CreateInstanceProfileOutput.
+error: An error if any issue occurs while trying to create the instance profile.
+
+---
+
+### AWSService.CreateRole(string)
+
+```go
+CreateRole(string) *iam.CreateRoleOutput, error
+```
+
+CreateRole creates a new role with the given name and assume role policy.
+
+**Parameters:**
+
+roleName: The name of the role to create.
+assumeRolePolicy: The policy that the role will assume.
+
+**Returns:**
+
+*iam.CreateRoleOutput: A pointer to the CreateRoleOutput.
+error: An error if any issue occurs while trying to create the role.
+
+---
+
+### AWSService.DeleteInstanceProfile(string)
+
+```go
+DeleteInstanceProfile(string) *iam.DeleteInstanceProfileOutput, error
+```
+
+DeleteInstanceProfile deletes an instance profile.
+
+**Parameters:**
+
+profileName: The name of the instance profile to delete.
+
+**Returns:**
+
+*iam.DeleteInstanceProfileOutput: A pointer to the DeleteInstanceProfileOutput.
+error: An error if any issue occurs while trying to delete the instance profile.
+
+---
+
+### AWSService.DeleteRole(string)
+
+```go
+DeleteRole(string) *iam.DeleteRoleOutput, error
+```
+
+DeleteRole deletes a role.
+
+**Parameters:**
+
+roleName: The name of the role to delete.
+
+**Returns:**
+
+*iam.DeleteRoleOutput: A pointer to the DeleteRoleOutput.
+error: An error if any issue occurs while trying to delete the role.
+
+---
+
+### AWSService.DeleteRolePolicy(string)
+
+```go
+DeleteRolePolicy(string) *iam.DeleteRolePolicyOutput, error
+```
+
+DeleteRolePolicy deletes a policy from a role.
+
+**Parameters:**
+
+roleName: The name of the role to delete the policy from.
+policyName: The name of the policy to delete.
+
+**Returns:**
+
+*iam.DeleteRolePolicyOutput: A pointer to the DeleteRolePolicyOutput.
+error: An error if any issue occurs while trying to delete the policy from the role.
+
+---
+
+### AWSService.DetachRolePolicy(string)
+
+```go
+DetachRolePolicy(string) *iam.DetachRolePolicyOutput, error
+```
+
+DetachRolePolicy detaches a policy from a role.
+
+**Parameters:**
+
+roleName: The name of the role to detach the policy from.
+policyArn: The ARN of the policy to detach.
+
+**Returns:**
+
+*iam.DetachRolePolicyOutput: A pointer to the DetachRolePolicyOutput.
+error: An error if any issue occurs while trying to detach the policy from the role.
+
+---
+
 ### AWSService.GetAWSIdentity()
 
 ```go
@@ -49,6 +206,47 @@ profileName: The name of the profile to retrieve.
 
 *types.InstanceProfile: A pointer to the InstanceProfile.
 error: An error if any issue occurs while trying to get the instance profile.
+
+---
+
+### AWSService.PutRolePolicy(string)
+
+```go
+PutRolePolicy(string) *iam.PutRolePolicyOutput, error
+```
+
+PutRolePolicy updates the policy for a role.
+
+**Parameters:**
+
+roleName: The name of the role to update the policy for.
+policyName: The name of the policy to update.
+policyDocument: The policy document to update.
+
+**Returns:**
+
+*iam.PutRolePolicyOutput: A pointer to the PutRolePolicyOutput.
+error: An error if any issue occurs while trying to update the policy for the role.
+
+---
+
+### AWSService.RemoveRoleFromInstanceProfile(string)
+
+```go
+RemoveRoleFromInstanceProfile(string) *iam.RemoveRoleFromInstanceProfileOutput error
+```
+
+RemoveRoleFromInstanceProfile removes a role from an instance profile.
+
+**Parameters:**
+
+profileName: The name of the instance profile to remove the role from.
+roleName: The name of the role to remove from the instance profile.
+
+**Returns:**
+
+*iam.RemoveRoleFromInstanceProfileOutput: A pointer to the RemoveRoleFromInstanceProfileOutput.
+error: An error if any issue occurs while trying to remove the role from the instance profile.
 
 ---
 
--- iam/iam.go
+++ iam/iam.go
@@ -27,8 +27,28 @@ type STSClientAPI interface {
 // **Attributes:**
 //
 // GetInstanceProfile: Function to get the instance profile.
+// CreateRole: Function to create a role.
+// AttachRolePolicy: Function to attach a policy to a role.
+// PutRolePolicy: Function to put a policy to a role.
+// CreateInstanceProfile: Function to create an instance profile.
+// AddRoleToInstanceProfile: Function to add a role to an instance profile.
+// DetachRolePolicy: Function to detach a policy from a role.
+// DeleteRolePolicy: Function to delete a policy from a role.
+// RemoveRoleFromInstanceProfile: Function to remove a role from an instance profile.
+// DeleteInstanceProfile: Function to delete an instance profile.
+// DeleteRole: Function to delete a role.
 type IdentityClientAPI interface {
 	GetInstanceProfile(ctx context.Context, params *iam.GetInstanceProfileInput, optFns ...func(*iam.Options)) (*iam.GetInstanceProfileOutput, error)
+	CreateRole(ctx context.Context, params *iam.CreateRoleInput, optFns ...func(*iam.Options)) (*iam.CreateRoleOutput, error)
+	AttachRolePolicy(ctx context.Context, params *iam.AttachRolePolicyInput, optFns ...func(*iam.Options)) (*iam.AttachRolePolicyOutput, error)
+	PutRolePolicy(ctx context.Context, params *iam.PutRolePolicyInput, optFns ...func(*iam.Options)) (*iam.PutRolePolicyOutput, error)
+	CreateInstanceProfile(ctx context.Context, params *iam.CreateInstanceProfileInput, optFns ...func(*iam.Options)) (*iam.CreateInstanceProfileOutput, error)
+	AddRoleToInstanceProfile(ctx context.Context, params *iam.AddRoleToInstanceProfileInput, optFns ...func(*iam.Options)) (*iam.AddRoleToInstanceProfileOutput, error)
+	DetachRolePolicy(ctx context.Context, params *iam.DetachRolePolicyInput, optFns ...func(*iam.Options)) (*iam.DetachRolePolicyOutput, error)
+	DeleteRolePolicy(ctx context.Context, params *iam.DeleteRolePolicyInput, optFns ...func(*iam.Options)) (*iam.DeleteRolePolicyOutput, error)
+	RemoveRoleFromInstanceProfile(ctx context.Context, params *iam.RemoveRoleFromInstanceProfileInput, optFns ...func(*iam.Options)) (*iam.RemoveRoleFromInstanceProfileOutput, error)
+	DeleteInstanceProfile(ctx context.Context, params *iam.DeleteInstanceProfileInput, optFns ...func(*iam.Options)) (*iam.DeleteInstanceProfileOutput, error)
+	DeleteRole(ctx context.Context, params *iam.DeleteRoleInput, optFns ...func(*iam.Options)) (*iam.DeleteRoleOutput, error)
 }
 
 // AWSService represents the AWS services needed for the application.
@@ -111,4 +131,240 @@ func (s *AWSService) GetInstanceProfile(profileName string) (*types.InstanceProf
 	}
 
 	return result.InstanceProfile, nil
+}
+
+// CreateRole creates a new role with the given name and assume role policy.
+//
+// **Parameters:**
+//
+// roleName: The name of the role to create.
+// assumeRolePolicy: The policy that the role will assume.
+//
+// **Returns:**
+//
+// *iam.CreateRoleOutput: A pointer to the CreateRoleOutput.
+// error: An error if any issue occurs while trying to create the role.
+func (s *AWSService) CreateRole(roleName, assumeRolePolicy string) (*iam.CreateRoleOutput, error) {
+	input := &iam.CreateRoleInput{
+		RoleName:                 aws.String(roleName),
+		AssumeRolePolicyDocument: aws.String(assumeRolePolicy),
+	}
+
+	result, err := s.IAMClient.CreateRole(context.Background(), input)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create role: %v", err)
+	}
+
+	return result, nil
+}
+
+// AttachRolePolicy attaches a policy to a role.
+//
+// **Parameters:**
+//
+// roleName: The name of the role to attach the policy to.
+// policyArn: The ARN of the policy to attach.
+//
+// **Returns:**
+//
+// *iam.AttachRolePolicyOutput: A pointer to the AttachRolePolicyOutput.
+// error: An error if any issue occurs while trying to attach the policy to the role.
+func (s *AWSService) AttachRolePolicy(roleName, policyArn string) (*iam.AttachRolePolicyOutput, error) {
+	input := &iam.AttachRolePolicyInput{
+		RoleName:  aws.String(roleName),
+		PolicyArn: aws.String(policyArn),
+	}
+
+	result, err := s.IAMClient.AttachRolePolicy(context.Background(), input)
+	if err != nil {
+		return nil, fmt.Errorf("failed to attach role policy: %v", err)
+	}
+
+	return result, nil
+}
+
+// PutRolePolicy updates the policy for a role.
+//
+// **Parameters:**
+//
+// roleName: The name of the role to update the policy for.
+// policyName: The name of the policy to update.
+// policyDocument: The policy document to update.
+//
+// **Returns:**
+//
+// *iam.PutRolePolicyOutput: A pointer to the PutRolePolicyOutput.
+// error: An error if any issue occurs while trying to update the policy for the role.
+func (s *AWSService) PutRolePolicy(roleName, policyName, policyDocument string) (*iam.PutRolePolicyOutput, error) {
+	input := &iam.PutRolePolicyInput{
+		RoleName:       aws.String(roleName),
+		PolicyName:     aws.String(policyName),
+		PolicyDocument: aws.String(policyDocument),
+	}
+
+	result, err := s.IAMClient.PutRolePolicy(context.Background(), input)
+	if err != nil {
+		return nil, fmt.Errorf("failed to put role policy: %v", err)
+	}
+
+	return result, nil
+}
+
+// CreateInstanceProfile creates a new instance profile with the given name.
+//
+// **Parameters:**
+//
+// profileName: The name of the instance profile to create.
+//
+// **Returns:**
+//
+// *iam.CreateInstanceProfileOutput: A pointer to the CreateInstanceProfileOutput.
+// error: An error if any issue occurs while trying to create the instance profile.
+func (s *AWSService) CreateInstanceProfile(profileName string) (*iam.CreateInstanceProfileOutput, error) {
+	input := &iam.CreateInstanceProfileInput{
+		InstanceProfileName: aws.String(profileName),
+	}
+
+	result, err := s.IAMClient.CreateInstanceProfile(context.Background(), input)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create instance profile: %v", err)
+	}
+
+	return result, nil
+}
+
+// AddRoleToInstanceProfile adds a role to an instance profile.
+//
+// **Parameters:**
+//
+// profileName: The name of the instance profile to add the role to.
+// roleName: The name of the role to add to the instance profile.
+//
+// **Returns:**
+//
+// *iam.AddRoleToInstanceProfileOutput: A pointer to the AddRoleToInstanceProfileOutput.
+// error: An error if any issue occurs while trying to add the role to the instance profile.
+func (s *AWSService) AddRoleToInstanceProfile(profileName, roleName string) (*iam.AddRoleToInstanceProfileOutput, error) {
+	input := &iam.AddRoleToInstanceProfileInput{
+		InstanceProfileName: aws.String(profileName),
+		RoleName:            aws.String(roleName),
+	}
+
+	result, err := s.IAMClient.AddRoleToInstanceProfile(context.Background(), input)
+	if err != nil {
+		return nil, fmt.Errorf("failed to add role to instance profile: %v", err)
+	}
+
+	return result, nil
+}
+
+// DetachRolePolicy detaches a policy from a role.
+//
+// **Parameters:**
+//
+// roleName: The name of the role to detach the policy from.
+// policyArn: The ARN of the policy to detach.
+//
+// **Returns:**
+//
+// *iam.DetachRolePolicyOutput: A pointer to the DetachRolePolicyOutput.
+// error: An error if any issue occurs while trying to detach the policy from the role.
+func (s *AWSService) DetachRolePolicy(roleName, policyArn string) (*iam.DetachRolePolicyOutput, error) {
+	input := &iam.DetachRolePolicyInput{
+		RoleName:  aws.String(roleName),
+		PolicyArn: aws.String(policyArn),
+	}
+	result, err := s.IAMClient.DetachRolePolicy(context.Background(), input)
+	if err != nil {
+		return nil, fmt.Errorf("failed to detach role policy: %v", err)
+	}
+	return result, nil
+}
+
+// DeleteRolePolicy deletes a policy from a role.
+//
+// **Parameters:**
+//
+// roleName: The name of the role to delete the policy from.
+// policyName: The name of the policy to delete.
+//
+// **Returns:**
+//
+// *iam.DeleteRolePolicyOutput: A pointer to the DeleteRolePolicyOutput.
+// error: An error if any issue occurs while trying to delete the policy from the role.
+func (s *AWSService) DeleteRolePolicy(roleName, policyName string) (*iam.DeleteRolePolicyOutput, error) {
+	input := &iam.DeleteRolePolicyInput{
+		RoleName:   aws.String(roleName),
+		PolicyName: aws.String(policyName),
+	}
+	result, err := s.IAMClient.DeleteRolePolicy(context.Background(), input)
+	if err != nil {
+		return nil, fmt.Errorf("failed to delete role policy: %v", err)
+	}
+	return result, nil
+}
+
+// RemoveRoleFromInstanceProfile removes a role from an instance profile.
+//
+// **Parameters:**
+//
+// profileName: The name of the instance profile to remove the role from.
+// roleName: The name of the role to remove from the instance profile.
+//
+// **Returns:**
+//
+// *iam.RemoveRoleFromInstanceProfileOutput: A pointer to the RemoveRoleFromInstanceProfileOutput.
+// error: An error if any issue occurs while trying to remove the role from the instance profile.
+func (s *AWSService) RemoveRoleFromInstanceProfile(profileName, roleName string) (*iam.RemoveRoleFromInstanceProfileOutput, error) {
+	input := &iam.RemoveRoleFromInstanceProfileInput{
+		InstanceProfileName: aws.String(profileName),
+		RoleName:            aws.String(roleName),
+	}
+	result, err := s.IAMClient.RemoveRoleFromInstanceProfile(context.Background(), input)
+	if err != nil {
+		return nil, fmt.Errorf("failed to remove role from instance profile: %v", err)
+	}
+	return result, nil
+}
+
+// DeleteInstanceProfile deletes an instance profile.
+//
+// **Parameters:**
+//
+// profileName: The name of the instance profile to delete.
+//
+// **Returns:**
+//
+// *iam.DeleteInstanceProfileOutput: A pointer to the DeleteInstanceProfileOutput.
+// error: An error if any issue occurs while trying to delete the instance profile.
+func (s *AWSService) DeleteInstanceProfile(profileName string) (*iam.DeleteInstanceProfileOutput, error) {
+	input := &iam.DeleteInstanceProfileInput{
+		InstanceProfileName: aws.String(profileName),
+	}
+	result, err := s.IAMClient.DeleteInstanceProfile(context.Background(), input)
+	if err != nil {
+		return nil, fmt.Errorf("failed to delete instance profile: %v", err)
+	}
+	return result, nil
+}
+
+// DeleteRole deletes a role.
+//
+// **Parameters:**
+//
+// roleName: The name of the role to delete.
+//
+// **Returns:**
+//
+// *iam.DeleteRoleOutput: A pointer to the DeleteRoleOutput.
+// error: An error if any issue occurs while trying to delete the role.
+func (s *AWSService) DeleteRole(roleName string) (*iam.DeleteRoleOutput, error) {
+	input := &iam.DeleteRoleInput{
+		RoleName: aws.String(roleName),
+	}
+	result, err := s.IAMClient.DeleteRole(context.Background(), input)
+	if err != nil {
+		return nil, fmt.Errorf("failed to delete role: %v", err)
+	}
+	return result, nil
 }
